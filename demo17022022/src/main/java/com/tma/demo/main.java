3268855e0eda4b41d05010d44480c4d7cf00abe2
Saokykg <52557205+Saokykg@users.noreply.github.com>
2022-02-17 17:25:02 +0700

    Add files via upload

---
+++ demo17022022/src/main/java/com/tma/demo/main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tma.demo;
+
+/**
+ *
+ * @author pduonghoa
+ */
+public class main {
+    public static void main(){
+        System.err.println("HELLO WORLD"); 
+    
+    }
+}
