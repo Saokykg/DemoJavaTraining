e8d7499050b69ec0c5dc7b6fffaf7a0bc20e19d5
Saokykg <52557205+Saokykg@users.noreply.github.com>
2022-03-11 06:18:03 +0700

    Add files via upload

---
+++ RestaurantManagementTMP/src/main/java/file/MenuPath.java
@@ -0,0 +1,13 @@
+package file;
+
+public class MenuPath {
+	private MenuPath() {
+		
+	}
+	public static final String url="src/main/java/file/";
+	public static final String urlBreakfastMenu=url+"BreakfastMenuData.csv";
+	public static final String urlLunchMenu=url+"LunchMenuData.csv";
+	public static final String urlDinnerMenu=url+"DinnerMenuData.csv";
+	public static final String urlSoftDrinkMenu=url+"SoftDrinkMenuData.csv";
+	public static final String urlAlcoholMenu=url+"AlcoholMenuData.csv";
+}
